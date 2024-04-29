2b674217bce9a3bb0bbeb28a07ace14137fb2857
adeleke-test <adeleke.david2017@mail.com>
2024-04-29 19:51:04 +0100

    Python class

---
+++ day.py
@@ -0,0 +1,14 @@
+#print("Hello David")
+#a = 10
+#print("a =", a)
+
+# 
+# Breaking of lines 
+print("Day 1 - Python Print Function \
+      \n The function is declared like this \
+      \n Print ('what to print')")
+
+#String concatenation methods
+print("Adeleke " + "David")
+print("Adeleke" + " " + "Darasimi")
+print("Adeleke" + " Damilola")
